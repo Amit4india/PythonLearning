0049bc9aa8cb067a6ca095a4013d59b6cd1b610c
AmitS <amit.itaas@gmail.com>
2017-08-22 17:21:37 +0530

    changing filereading method

--- LearningPython/FileReadingWriting/FileReadingExceptionHandling.py
+++ LearningPython/FileReadingWriting/FileReadingExceptionHandling.py
@@ -6,8 +6,8 @@ Created on Aug 18, 2017
 def FileRead():
     # this function define the method of how to open file and protect it using try Block
     try:
-        file = open("C:\Eclipse\Workspace\LearningPython\SampleReadFile", 'r+')
-        if file.name=="C:\Eclipse\Workspace\LearningPython\SampleReadFile":
+        file = open("C:\Eclipse\Workspace\PythonLearning\LearningPython\SampleReadFile", 'r+')
+        if file.name=="C:\Eclipse\Workspace\PythonLearning\LearningPython\SampleReadFile":
             print(file.name)
         else:
             raise Exception
@@ -17,7 +17,7 @@ def FileRead():
         print ("file name is not matched")
     else: # this piece of code will only execute if try does through an error!
         file.write("hello amit how are you \n. We will become python master someday")
-        print(file.readlines())
+        print(file.read())
     finally: # This piece of code always run that's why we have put it close file in this block
         file.close
     return
